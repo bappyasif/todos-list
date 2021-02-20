763e93f140bc27764ec5af7891e57d5f866e1f66
bappyasif <bappyasif@hotmail.com>
2021-02-21 02:04:20 +0600

    Added to previous commit wth update for Todo List Assignemnt, curently task entry is in place for taking it to next level for a functioning Todo List.

--- js/properAttemptForAssignment/addTasks/initiateEntry.js
+++ js/properAttemptForAssignment/addTasks/initiateEntry.js
@@ -1,12 +1,21 @@
+import { startOfToday, format, endOfDay } from "date-fns";
+import { showPriorities, showSelectDDElem } from "./prioritiyLevels.js";
+let taskDate, taskDue, taskPriority, taskNote;
+taskDate =  document.querySelector(".dump-calVal");
+taskDue = document.querySelector(".dump-ddVal");
+
 function readyTodoTaskEntry() {
     let addTask = document.querySelector("#show-panel");
     let entryTask = document.querySelector(".task-entry");
     let addTodo = document.querySelector(".todo-task");
     addTask.addEventListener("click", () => {
-        if(entryTask.style.display === "none") {
+        if (entryTask.style.display === "none") {
             entryTask.style.display = "block";
-            addTodo.addEventListener("click", () => {
-                getUserTaskEntry();
+            addTodo.addEventListener("click", (evt) => {
+                // getUserTaskEntry();
+                // if (evt.target.id === "calendar") taskDate = datePicker.value;
+                // else if (evt.target.id === "due-date") taskDue = datePicker.value;
+                console.log(taskDate, taskDue);
             })
         } else {
             entryTask.style.display = "none";
@@ -14,35 +23,119 @@ function readyTodoTaskEntry() {
     });
 }
 
+getUserTaskEntry();
+getValuesFromIcons();
+
 function getUserTaskEntry() {
     let taskInput = document.querySelector("#create-task");
     let taskTitle = taskInput.value;
     console.log(taskTitle);
-    getValuesFromIcons();
+    // getValuesFromIcons();
 }
 
 function getValuesFromIcons() {
+    let calendar = document.querySelector("#calendar");
+    showDatePickerElement(calendar);
+    let dueDate = document.querySelector("#due-date");
+    showDatePickerElement(dueDate);
+    showPriorities();
+    showSelectDDElem();
+    showAndGetNotes();
+}
+
+function showDatePickerElement(htmlElement) {
+    htmlElement.addEventListener("click", () => {
+        let datePicker = document.querySelector("#date-picker");
+        datePicker.value = format(startOfToday(), "yyyy-MM-dd");
+        if (datePicker.style.display === "none") {
+            datePicker.style.display = "block";
+            if(htmlElement.id==="calendar") {
+                // datePicker.addEventListener("change", () => {
+                //     taskDate.textContent =  datePicker.value;
+                // });
+                taskDate.textContent =  datePicker.value;
+            } else if(htmlElement.id === "due-date") {
+                datePicker.addEventListener("change", () => {
+                    taskDue.textContent = datePicker.value;
+                });
+                // taskDue.textContent = datePicker.value;
+            }
+            // datePicker.value = format(startOfToday(), "yyyy-MM-dd");
+            
+        } else {
+            datePicker.value = "";
+            datePicker.style.display = "none";
+        }
+    });
+}
+
+function showAndGetNotes() {
+    let notesIcon = document.querySelector("#notes");
+    let noteInput = document.querySelector("#about-task");
+
+    notesIcon.addEventListener("click", () => {
+        if (noteInput.style.display === "none") {
+            noteInput.style.display = "block";
+        } else {
+            noteInput.style.display = "none";
+        }
+    });
+}
+
+export { readyTodoTaskEntry }
+
+
+/**
+ *
+ * 
+ function showDatePickerElement(htmlElement) {
+    htmlElement.addEventListener("click", () => {
+        let datePicker = document.querySelector("#date-picker");
+        datePicker.value = format(startOfToday(), "yyyy-MM-dd");
+        if (datePicker.style.display === "none") {
+            datePicker.style.display = "block";
+            if(htmlElement.id==="calendar") {
+                // datePicker.value = format(startOfToday(), "yyyy-MM-dd");
+                taskDate.textContent =  datePicker.value;
+            } else if(htmlElement.id === "due-date") {
+                // datePicker.value = format(endOfDay(), "yyyy-MM-dd");
+                // datePicker.value = endOfDay(datePicker.value);
+                datePicker.value = format(startOfToday(), "yyyy-MM-dd");
+                // datePicker.value = endOfDay(new Date(2021,2,20));
+                // datePicker.value = endOfDay(new Date());
+                taskDue.textContent = datePicker.value;
+            }
+            // datePicker.value = format(startOfToday(), "yyyy-MM-dd");
+            
+        } else {
+            datePicker.value = "";
+            datePicker.style.display = "none";
+        }
+    });
+}
+ * 
+ *
+ function getValuesFromIcons() {
     let calendar =  document.querySelector("#calendar");
     // console.log(calendar);
     calendar.addEventListener("click", () => {
-        console.log("?!");
+        // console.log("?!");
         let datePicker = document.querySelector("#date-picker");
         if(datePicker.style.display === "none") {
             datePicker.style.display = "block";
             // datePicker.innerHTML = ""
+            // console.log(startOfToday());
+            // datePicker.value = startOfToday();
+            // datePicker.value = "2021-02-20";
+            datePicker.value = format(startOfToday(), "yyyy-MM-dd");
         } else {
             datePicker.style.display = "none";
         }
-        
+
     })
 }
-
-export {readyTodoTaskEntry}
-
-
-/**
- * 
- * 
+ *
+ *
  function readyTodoTaskEntry() {
     let addTask = document.querySelector("#show-panel");
     let entryTask = document.querySelector(".task-entry");
---
+++ js/properAttemptForAssignment/addTasks/prioritiyLevels.js
@@ -0,0 +1,74 @@
+let priorities = ["Normal", "Low", "Medium", "High"];
+let priorityLevels =  document.querySelector("#levels");
+
+function showPriorities() {
+    for(let i=0; i<priorities.length; i++) {
+        let option =  document.createElement("option");
+        option.value = priorities[i];
+        option.textContent = priorities[i];
+        option.classList.add(priorities[i].toLocaleLowerCase());
+        priorityLevels.append(option);
+    }
+}
+
+function showSelectDDElem() {
+    let priority =  document.querySelector("#priority");
+    priority.addEventListener("click", () => {
+        if(priorityLevels.style.display === "none") {
+            priorityLevels.style.display = "block";
+        } else {
+            priorityLevels.style.display = "none";
+        }
+    });
+}
+
+export {showPriorities, showSelectDDElem}
+
+
+/**
+ *
+ * 
+ function showPriorities() {
+    // let priorityLevels =  document.querySelector("#levels");
+    for(let i=0; i<priorities.length; i++) {
+        let option =  document.createElement("option");
+        option.value = priorities[i];
+        option.textContent = priorities[i];
+        // let colorHint = document.createElement("div");
+        // colorHint.classList.add("option-elem");
+        // option.append(colorHint);
+        option.classList.add(priorities[i].toLocaleLowerCase());
+        priorityLevels.append(option);
+    }
+}
+ *  
+ * 
+ function showSelectDDElem() {
+    let priority =  document.querySelector("#priority");
+    priority.addEventListener("click", () => {
+        // console.log(priorityLevels.style.display, "<>", window.getComputedStyle(priorityLevels).display);
+        if(priorityLevels.style.display === "none") {
+            // console.log("::")
+            priorityLevels.style.display = "block";
+            // priorityLevels.style.display = "auto";
+        } else {
+            // console.log(";;")
+            priorityLevels.style.display = "none";
+        }
+        // if(priorityLevels.style.display === "none") {
+        //     // console.log("::")
+        //     priorityLevels.style.display === "block";
+        // } else {
+        //     // console.log(";;")
+        //     priorityLevels.style.display === "none";
+        // }
+        // if(window.getComputedStyle(priorityLevels).display === "none") {
+        //     console.log("::", priorityLevels);
+        //     window.getComputedStyle(priorityLevels).display === "block";
+        // } else {
+        //     // console.log(";;")
+        //     window.getComputedStyle(priorityLevels).display === "none";
+        // }
+    });
+}
+ */
